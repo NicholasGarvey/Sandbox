4f68f0bf6e2cfe4be62241c368d652563805718e
Nicholas Garvey <nicholas.garvey@jcu.edu.au>
2019-08-29 10:39:17 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,12 @@
 # Hi this is Nicholas
+password = input("Please enter a password")
+count_lower = 0
+count_upper = 0
+for char in password:
+    if char.islower():
+        count_lower += 1
+    elif char.isupper():
+        count_upper += 1
+if count_lower >= 10 or count_upper >= 10:
+    for char in password:
+        print("*", end='')
