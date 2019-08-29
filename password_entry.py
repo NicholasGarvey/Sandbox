d6d5d6b53c0e5d691c3739b9ed7ac8e738cb1c5e
Nicholas Garvey <nicholas.garvey@jcu.edu.au>
2019-08-29 11:27:59 +1000

    add password checker program

--- password_entry.py
+++ password_entry.py
@@ -3,5 +3,4 @@ min_length = 10
 password = input("Please enter a password with {} characters".format(min_length))
 while len(password) < min_length:
     password = input("Enter password of at least {} characters: ".format(min_length))
-
 print('*' * len(password))
