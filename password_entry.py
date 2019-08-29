25941f58ff5adf82452cd8840ce0a22778d694d4
Nicholas Garvey <nicholas.garvey@jcu.edu.au>
2019-08-29 11:26:00 +1000

    add password code

--- password_entry.py
+++ password_entry.py
@@ -1,12 +1,7 @@
 # Hi this is Nicholas
-password = input("Please enter a password")
-count_lower = 0
-count_upper = 0
-for char in password:
-    if char.islower():
-        count_lower += 1
-    elif char.isupper():
-        count_upper += 1
-if count_lower >= 10 or count_upper >= 10:
-    for char in password:
-        print("*", end='')
+min_length = 10
+password = input("Please enter a password with {} characters".format(min_length))
+while len(password) < min_length:
+    password = input("Enter password of at least {} characters: ".format(min_length))
+
+print('*' * len(password))
